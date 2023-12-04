91fdbd9729cc98847ae059b9466d9a5d2fc7087d
leienci <leienci@qq.com>
2023-12-04 18:20:35 +0800

    feat:使用BeautifulSoup

--- spider/che168.py
+++ spider/che168.py
@@ -1,15 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+# 请求头
 headers = {
     'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36'
 }
-
 url = 'https://www.che168.com/xiamen/list/#pvareaid=100943'
 
+# 接收请求响应文件
 response = requests.get(url=url, headers=headers)
-
-data_html = response.text
-# print(data_html)
-
+soup_details = BeautifulSoup(response.text, 'html.parser')
+print(soup_details)
 
