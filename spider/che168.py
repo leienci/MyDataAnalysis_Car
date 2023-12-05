6fb297d36eb3094630df4ac2b519635f0c3ad089
leienci <leienci@qq.com>
2023-12-05 15:41:09 +0800

    feat:使用parsel爬取

--- spider/che168.py
+++ spider/che168.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+import parsel
 
 # 请求头
 headers = {
@@ -9,6 +9,24 @@ url = 'https://www.che168.com/xiamen/list/#pvareaid=100943'
 
 # 接收请求响应文件
 response = requests.get(url=url, headers=headers)
-soup_details = BeautifulSoup(response.text, 'html.parser')
-print(soup_details)
+data_html = response.text
+# print(data_html)
 
+# 筛选数据
+selector = parsel.Selector(data_html)
+lis = selector.css('.viewlist_ul li')
+for li in lis:
+    try:
+        name = li.css('.card-name::text').get()  # 车名
+        unit = li.css('.cards-unit::text').get()  # 信息
+        kmNum = unit.split('／')[0]
+        years = unit.split('／')[1]
+        city = unit.split('／')[2]
+        business = unit.split('／')[3]
+        price = li.css('.pirce em::text').get()  # 售价
+        yprice = li.css('s::text').get()  # 原价
+        carinfo = li.css('.carinfo::attr(href)').get()
+        img = li.css('img::attr(src)').get()
+        print(name, kmNum, years, city, business, price, yprice)
+    except:
+        pass
