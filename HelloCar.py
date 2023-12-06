01ddad1e7f8ae81c2e80b6b7b92cefaa372a179a
leienci <leienci@qq.com>
2023-12-06 20:55:01 +0800

    feat:多线程

--- HelloCar.py
+++ HelloCar.py
@@ -1,1 +1,15 @@
-print('Hello,Car!')
+import time
+import threading
+
+
+def worker(name):
+    for i in range(5):
+        print(name, i)
+        time.sleep(0.5)
+
+
+t1 = threading.Thread(target=worker, args=('计数器A',))
+t1.start()
+t2 = threading.Thread(target=worker, args=('计数器B',))
+t2.start()
+print('完成')
