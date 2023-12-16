21be8a264fc3650115111cd5b7946bd7889c24ca
leienci <leienci@qq.com>
2023-12-16 21:40:12 +0800

    feat:二手车页面分页显示车辆

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ import sqlite3
 
 app = Flask(__name__)
 
+@app.template_filter()
+def minus(value, arg):
+    return value - arg
+
+@app.template_filter()
+def plus(value, arg):
+    return value + arg
+
 
 @app.route('/index')
 def home():
@@ -33,25 +41,40 @@ def home():
 
 @app.route('/car')
 def movie():
-    # datalist = []
-    # con = sqlite3.connect("che168.db")
-    # cur = con.cursor()
-    # sql = "select * from che168"
-    # data = cur.execute(sql)
-    # for item in data :
-    #     datalist.append(item)
-    # cur.close()
-    # con.close()
-    #
-    # # 当前页码，从第一页开始
-    # page = int(request.args.get("page", 1))
-    # # 每页的数量
-    # per_page = int(request.args.get('per_page', 25))
-    # paginate = data.paginate(page, per_page, error_out=False)
-    #
-    # # return render_template('car.html',movies = datalist,paginate=paginate)
+    datalist = []
+    con = sqlite3.connect("che168.db")
+    cur = con.cursor()
+    sql = "select * from che168"
+    data = cur.execute(sql)
+    for item in data:
+        datalist.append(item)
+
+    page = int(request.args.get('page', 1))
+    per_page = 100
+
+    # 计算偏移量
+    offset = (page - 1) * per_page
+
+    # 执行查询
+    cur.execute(f"SELECT * FROM che168 LIMIT {per_page} OFFSET {offset}")
+    data = cur.fetchall()
+
+    # 获取总数据条数
+    cur.execute("SELECT COUNT(*) FROM che168")
+    total_count = cur.fetchone()[0]
+
+    # 计算总页数
+    total_pages = total_count // per_page + (1 if total_count % per_page > 0 else 0)
+
+    # 关闭数据库连接
+    cur.close()
+    con.close()
+
+    return render_template('car.html', cars=data, page=page, per_page=per_page, total_pages=total_pages)
+
+    # return render_template('car.html',movies = datalist,paginate=paginate)
     # return render_template('car.html',cars = datalist)
-    return render_template('car.html')
+    # return render_template('car.html')
 
 
 if __name__ == '__main__':
