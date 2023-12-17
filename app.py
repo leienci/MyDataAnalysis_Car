e7e568ec2564336a98e93ce206b8baa9d8155ecf
leienci <leienci@qq.com>
2023-12-17 13:30:47 +0800

    feat:车辆级别页面

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import request
 from flask import Flask
 from flask import render_template
 
+import json
 import sqlite3
 
 app = Flask(__name__)
 
+
 @app.template_filter()
 def minus(value, arg):
     return value - arg
+
 
 @app.template_filter()
 def plus(value, arg):
@@ -40,7 +43,7 @@ def home():
 
 
 @app.route('/car')
-def movie():
+def car():
     datalist = []
     con = sqlite3.connect("che168.db")
     cur = con.cursor()
@@ -50,7 +53,7 @@ def movie():
         datalist.append(item)
 
     page = int(request.args.get('page', 1))
-    per_page = 100
+    per_page = 50  # 每页显示数量
 
     # 计算偏移量
     offset = (page - 1) * per_page
@@ -75,6 +78,24 @@ def movie():
     # return render_template('car.html',movies = datalist,paginate=paginate)
     # return render_template('car.html',cars = datalist)
     # return render_template('car.html')
+
+
+@app.route('/level')
+def level():
+    level = []  # 车辆等级
+    num = []  # 每一个等级对应的车辆数量
+    con = sqlite3.connect("che168.db")
+    cur = con.cursor()
+    sql = "select 车辆级别, count(车辆级别) from che168 where 车辆级别 != 'None' group by 车辆级别"
+    data = cur.execute(sql)
+    for item in data:
+        level.append(str(item[0]))
+        num.append(item[1])
+        print(level, num)
+
+    cur.close()
+    con.close()
+    return render_template('level.html', level=level, num=num)
 
 
 if __name__ == '__main__':
